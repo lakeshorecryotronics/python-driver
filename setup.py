f3e3537fa526805123e8630c5f4c573dc95d1975
Houston Fortney <Houston.Fortney@lakeshore.com>
2018-10-15 17:55:12 -0400

    Add python package and setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name='lakeshore',
+    version='0.0.1',
+    packages=[r'lakeshore'],
+    url='',  # TODO: Add GitHub URL
+    install_requires=[]  # TODO: Add dependencies
+)
