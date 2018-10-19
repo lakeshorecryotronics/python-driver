bf9d9b6de7d636605510cc5b409f5cddafa76512
Noah.Faust <noah.faust@lakeshore.com>
2018-10-19 10:11:45 -0400

    Added 155 and M91 classes

---
+++ lakeshore/FastHall.py
@@ -0,0 +1,13 @@
+"""Implements functionality unique to the Lake Shore M91 Fast Hall"""
+
+from .XIPInstrument import XIPInstrument
+
+
+class PrecisionSource(XIPInstrument):
+    """A XIP Instrument subclass that establishes M91 specific parameters and methods"""
+
+    vid_pid = [(0x1FB9, 0x0704)]
+
+    def __init__(self, serial_number=None, com_port=None, baud_rate=115200, timeout=2.0, flow_control=True):
+        # Call the parent init, then fill in values specific to the Teslameter
+        XIPInstrument.__init__(self, serial_number, com_port, baud_rate, timeout, flow_control)
