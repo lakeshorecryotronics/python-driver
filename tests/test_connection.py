3563ab986a5845259b8be425df1cbeba60d43282
Houston Fortney <Houston.Fortney@lakeshore.com>
2018-10-22 13:52:10 -0400

    Implement basic tests.

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,6 +1,28 @@
 import unittest
 
+# Teslameter is used for these general tests on the HIL rig at this time
+from lakeshore import Teslameter, XIPInstrumentConnectionException
 
-class TestSomething(unittest.TestCase):
-    def test_that_something_does_something_useful(self):
-        pass  # TODO: Implement tests
+
+class TestDiscovery(unittest.TestCase):
+    def test_normal_connection(self):
+        Teslameter(flow_control=False)  # No checks needed, just make sure no exceptions are thrown
+
+    def test_target_does_not_exist(self):
+        with self.assertRaises(XIPInstrumentConnectionException):
+            Teslameter(serial_number='Fake', flow_control=False)
+
+
+class TestConnectivity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dut = Teslameter(flow_control=False)  # TODO: Get a dut with flow control for the HIL rig then remove this.
+
+    def test_basic_query(self):
+        response = self.dut.query('*IDN?')
+
+        self.assertEqual(response.split(',')[0], 'Lake Shore')
+
+    def test_timeout(self):
+        with self.assertRaises(XIPInstrumentConnectionException):
+            self.dut.query('FAKEQUERY?')
