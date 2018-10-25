06b9048485c7b2811ab3c7a1a6b98dc07527c661
Noah.Faust <noah.faust@lakeshore.com>
2018-10-25 11:08:51 -0400

    Added ip address parameter to the instrument classes

--- lakeshore/precision_source.py
+++ lakeshore/precision_source.py
@@ -8,6 +8,11 @@ class PrecisionSource(XIPInstrument):
 
     vid_pid = [(0x1FB9, 0x0103)]
 
-    def __init__(self, serial_number=None, com_port=None, baud_rate=115200, timeout=2.0, flow_control=True):
+    def __init__(self, serial_number=None,
+                 com_port=None,
+                 baud_rate=115200,
+                 timeout=2.0,
+                 flow_control=True,
+                 ip_address=None):
         # Call the parent init, then fill in values specific to the 155
-        XIPInstrument.__init__(self, serial_number, com_port, baud_rate, timeout, flow_control)
+        XIPInstrument.__init__(self, serial_number, com_port, baud_rate, timeout, flow_control, ip_address)
