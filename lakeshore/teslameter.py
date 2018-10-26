b978bf7ac0edf185be3377d1d7b159df05622c0a
Noah.Faust <noah.faust@lakeshore.com>
2018-10-26 10:47:31 -0400

    Added an option to write data to a file

--- lakeshore/teslameter.py
+++ lakeshore/teslameter.py
@@ -21,8 +21,8 @@ class Teslameter(XIPInstrument):
         # Call the parent init, then fill in values specific to the Teslameter
         XIPInstrument.__init__(self, serial_number, com_port, baud_rate, flow_control, timeout, ip_address)
 
-    def get_buffered_data(self, length_of_time_in_seconds, sample_rate_in_ms=None):
-        """Returns an array of parsed field strength and input state data"""
+    def get_buffered_data(self, length_of_time_in_seconds, sample_rate_in_ms=None, file_name=None):
+        """Returns an array of parsed field and input state data. It optionally writes the data to a csv file"""
 
         # Make the amount of time a whole number
         length_of_time_in_seconds = round(length_of_time_in_seconds, 2)
@@ -37,6 +37,12 @@ class Teslameter(XIPInstrument):
         self.query('FETC:BUFF:DC?', check_errors=False)
 
         buffered_data = []
+        
+        # Create a csv file with headers if a file name is passed. Files of the same name will be overwritten.
+        if file_name is not None:
+            file = open(file_name + ".csv", "w")
+            file.write('time elapsed,date,hour,minute,second,time zone hour,time zone minute,' +
+                       'magnitude,x,y,z,field control set point,input state\n')
 
         # Loop until the designated amount of time has been reached
         while True:
@@ -57,11 +63,21 @@ class Teslameter(XIPInstrument):
                         parsed_point.append('0')
                         parsed_point.append(input_state)
 
+                    # Calculate how much time has passed.
+                    elapsed_time = (len(buffered_data) + 1) * sample_rate_in_ms / 1000
+
                     # Unpack the parsed point into a namedtuple and append it to the list
-                    new_point = DataPoint((len(buffered_data) + 1) * sample_rate_in_ms / 1000,
-                                          *parsed_point)
+                    new_point = DataPoint(elapsed_time, *parsed_point)
                     buffered_data.append(new_point)
+
+                    # Write the data to the file in a csv friendly format if a file name was provided
+                    if file_name is not None:
+                        file.write(str(elapsed_time) + ',' +
+                                   str(parsed_point).replace("'", "").replace("[", "").replace("]", "") +
+                                   '\n')
 
                     # Check to see if time is up. If so, return the data.
                     if len(buffered_data) * sample_rate_in_ms >= length_of_time_in_seconds * 1000:
+                        if file_name is not None:
+                            file.close()
                         return buffered_data
