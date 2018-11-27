21a75fec527fbb63c000fe2fc81b8e64df64b013
Noah.Faust <noah.faust@lakeshore.com>
2018-11-27 11:43:05 -0500

    Added functools wraps to decorator

--- lakeshore/requires_firmware_version.py
+++ lakeshore/requires_firmware_version.py
@@ -1,5 +1,6 @@
 """Decorator for ensuring an error is raised if the instrument firmware does not support the called function."""
 
+import functools
 from distutils.version import LooseVersion
 
 from lakeshore.xip_instrument import XIPInstrumentConnectionException
@@ -10,8 +11,8 @@ def requires_firmware_version(required_version):
     is not up to date with the function's required version."""
 
     def decorator_version_check(func):
+        @functools.wraps(func)
         def wrapper(self, *args, **kwargs):
-
             # Raise an error if the instrument version is earlier than the required version.
             if LooseVersion(required_version) > LooseVersion(self.firmware_version):
                 raise XIPInstrumentConnectionException(func.__name__ + ' requires instrument firmware version ' +
