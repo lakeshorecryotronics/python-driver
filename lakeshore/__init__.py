79295fce0bb0d97e827e6e06063c22a0b45075f6
Noah Faust <Noah.Faust@lakeshore.com>
2019-12-12 13:57:21 -0500

    Add classes for most existing Lake Shore instruments

--- lakeshore/__init__.py
+++ lakeshore/__init__.py
@@ -5,3 +5,17 @@ from .M91 import FastHall, FastHallOperationRegister, FastHallQuestionableRegist
     ContactCheckOptimizedParameters, FastHallManualParameters, FastHallLinkParameters, FourWireParameters,\
     DCHallParameters, ResistivityManualParameters, ResistivityLinkParameters, M91
 from .model_155 import PrecisionSource, PrecisionSourceOperationRegister, PrecisionSourceQuestionableRegister, Model155
+from .model_121 import Model121
+from .model_218 import Model218
+from .model_224 import Model224
+from .model_240 import Model240
+from .model_335 import Model335
+from .model_336 import Model336
+from .model_350 import Model350
+from .model_372 import Model372
+from .model_425 import Model425
+from .model_455 import Model455
+from .model_475 import Model475
+from .model_625 import Model625
+from .model_643 import Model643
+from .model_648 import Model648
---
+++ lakeshore/model_218.py
@@ -0,0 +1,26 @@
+from .generic_instrument import GenericInstrument
+import serial
+
+
+class Model218(GenericInstrument):
+    # A class object representing the Lake Shore model 218 temperature monitor
+
+    vid_pid = [(0x1FB9, 0x0200)]
+
+    def __init__(self,
+                 serial_number=None,
+                 com_port=None,
+                 baud_rate=9600,
+                 data_bits=9,
+                 stop_bits=1,
+                 parity=serial.PARITY_NONE,
+                 flow_control=False,
+                 handshaking=False,
+                 timeout=2.0,
+                 ip_address=None,
+                 tcp_port=7777,
+                 **kwargs):
+
+        # Call the parent init, then fill in values specific to the 121
+        GenericInstrument.__init__(serial_number, com_port, baud_rate, data_bits, stop_bits, parity, flow_control,
+                                   handshaking, timeout, ip_address, tcp_port, **kwargs)
