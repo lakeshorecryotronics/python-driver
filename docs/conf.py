33579bab8b9db5447dfec38688902e68447060ca
Noah Faust <Noah.Faust@lakeshore.com>
2020-01-08 14:17:10 -0500

    Remove uneeded header and footer information

--- docs/conf.py
+++ docs/conf.py
@@ -203,11 +203,11 @@ html_sidebars = {'**': ['globaltoc.html', 'relations.html', 'sourcelink.html', '
 
 # If true, links to the reST sources are added to the pages.
 #
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 #
-# html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #
